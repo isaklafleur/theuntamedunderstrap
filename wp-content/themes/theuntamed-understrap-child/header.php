b5e99f38d0645e01f4ab552de8ad3d2119fe916d
Isak Engdahl <isak.engdahl@gmail.com>
2021-10-30 00:47:20 +0200

    updated bootstrap code

---
+++ wp-content/themes/theuntamed-understrap-child/header.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
+    <?php do_action( 'wp_body_open' ); ?>
+    <div class="site" id="page">
+
+
+
+
+        <!-- ******************* The Navbar Area ******************* -->
+        <div class="navbar smart-scroll d-flex justify-content-between align-items-center bg-primary">
+            <div class="p-3"><a href="#" class="primarynavlinks">MENU+</a></div>
+            <div class="p-3"><img src="https://uploads-ssl.webflow.com/615b085387c00df1534c8d44/6166e64aed05c62b2e51f093_The_Untamed_Logo_210705_white-p-500.png" class="p-0" alt="The Untamed Logo"
+                    width="110" height="46"></div>
+            <div class="p-3"><a href="#" class="primarynavlinks">ACCOUNT</a> <a href="#" class="primarynavlinks pl-3">BAG 00</a></div>
+        </div>
+        <!-- #wrapper-navbar end -->
--- wp-content/themes/theuntamed-understrap-child/js/child-theme.js
+++ wp-content/themes/theuntamed-understrap-child/js/child-theme.js
@@ -9721,6 +9721,26 @@
     }
   })();
 
+  // Add your custom JS here.
+  // add padding top to show content behind navbar
+  jQuery('body').css('padding-top', jQuery('.navbar').outerHeight() + 'px'); // detect scroll top or down
+
+  if (jQuery('.smart-scroll').length > 0) {
+    // check if element exists
+    var last_scroll_top = 0;
+    jQuery(window).on('scroll', function () {
+      var scroll_top = jQuery(this).scrollTop();
+
+      if (scroll_top < last_scroll_top) {
+        jQuery('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+      } else {
+        jQuery('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+      }
+
+      last_scroll_top = scroll_top;
+    });
+  }
+
   exports.Alert = Alert;
   exports.Button = Button;
   exports.Carousel = Carousel;
