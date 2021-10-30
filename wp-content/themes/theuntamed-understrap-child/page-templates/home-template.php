3e18efe696374a023304ca494d4cf549d9f275f1
Isak Engdahl <isak.engdahl@gmail.com>
2021-10-30 22:38:26 +0200

    Improvements in ux toolbox

---
+++ wp-content/themes/theuntamed-understrap-child/page-templates/home-template.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: Home Page Template
+ *
+ * Template for displaying a page just with the header and footer area and a "naked" content area in between.
+ * Good for landingpages and other types of pages where you want to add a lot of custom markup.
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+?>
+<div class="_hm_banner">
+    <div class="_hm_banner_imgwpr">
+        <img src="https://staging-theuntamedmultisite.kinsta.cloud/wp-content/uploads/2021/09/alessio-albi-portrait-12.png" alt="">
+    </div>
+
+</div>
+<?php get_footer(); ?>
